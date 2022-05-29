cff42365540452b8f6c6d73b9ae0088c9e3f3d18
PeterOnCode <peteroncode@gmail.com>
2022-05-29 09:19:57 +0200

    [chapter-02] - 01 - Hello JSX

---
+++ Chapter-2/01-hello-jsx/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <section>
+      <h1>My Component</h1>
+      <p>Content in my component ...</p>
+    </section>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# react-and-react-native
+# React and React Native
+
+## Chapter 1
+
+```shell
+npx create-react-app 01-hello-jsx
+```
